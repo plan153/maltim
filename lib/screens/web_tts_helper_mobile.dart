04c90097a2a515787049272faf9e0d1817502077
plan153 <plan153@gmail.com>
2026-06-13 08:45:36 +0000

    chore: 잘못 복사된 파일 제거

--- lib/screens/web_tts_helper_mobile.dart
+++
@@ -1,8 +0,0 @@
-class WebTtsHelper {
-  static Future<void> playAzureTts(String text, String key, String region,
-      String voice, double rateMultiplier, [String? lang]) async {}
-  static Future<void> stopAzureTts() async {}
-  static Future<void> playChime() async {}
-  static void unlockAudio() {}
-  static void setMicActive(bool active) {}
-}
