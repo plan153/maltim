ba484fb41eccf4cfa337d301f28db81e53b23a2b
plan153 <plan153@gmail.com>
2026-06-14 06:31:45 +0000

    fix: practice_screen.dart에 kIsWeb 사용을 위한 foundation import 추가 (빌드 오류 수정)

--- lib/screens/practice_screen.dart
+++ lib/screens/practice_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:pronunciation_engine/pronunciation_engine.dart';
